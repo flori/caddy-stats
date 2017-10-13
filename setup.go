413e9ad4e048e521ec4543673f09b1a15bf9face
Florian Frank <flori@ping.de>
2017-10-13 11:22:55 +0200

    Restrict max size of LOG set

--- setup.go
+++ setup.go
@@ -1,9 +1,29 @@
 package stats
 
 import (
+	"log"
+	"os"
+	"strconv"
+
 	"github.com/mholt/caddy"
 	"github.com/mholt/caddy/caddyhttp/httpserver"
 )
+
+var lms = 10000
+
+func logMaxSize() int {
+	size, ok := os.LookupEnv(LOG_MAX_SIZE_VARIABLE)
+	if !ok {
+		goto Default
+	}
+	if s, err := strconv.Atoi(size); err == nil {
+		if s >= 0 {
+			return s
+		}
+	}
+Default:
+	return 10000
+}
 
 func setup(c *caddy.Controller) error {
 	config := httpserver.GetConfig(c)
@@ -13,13 +33,15 @@ func setup(c *caddy.Controller) error {
 		ru = args[1]
 	}
 	middleware := func(next httpserver.Handler) httpserver.Handler {
-		return StatsHandler{RedisURL: ru, Next: next}
+		return StatsHandler{RedisURL: ru, LogMaxSize: lms, Next: next}
 	}
 	config.AddMiddleware(middleware)
 	return nil
 }
 
 func init() {
+	lms = logMaxSize()
+	log.Printf("Setting up caddy-stats middleware with LOG_MAX_SIZE = %d", lms)
 	caddy.RegisterPlugin("stats", caddy.Plugin{
 		ServerType: "http",
 		Action:     setup,
--- stats.go
+++ stats.go
@@ -1,20 +1,26 @@
 package stats
 
 import (
-	"github.com/flori/caddy-stats/log_json"
-	"github.com/go-redis/redis"
-	"github.com/mholt/caddy/caddyhttp/httpserver"
 	"log"
 	"net/http"
 	"os"
+
+	"github.com/flori/caddy-stats/log_json"
+	"github.com/go-redis/redis"
+	"github.com/mholt/caddy/caddyhttp/httpserver"
+)
+
+const (
+	REDIS_URL_VARIABLE    = "REDIS_URL"
+	LOG_MAX_SIZE_VARIABLE = "LOG_MAX_SIZE"
+	LOG_SET               = "LOG"
 )
 
 type StatsHandler struct {
-	RedisURL string
-	Next     httpserver.Handler
+	RedisURL   string
+	LogMaxSize int
+	Next       httpserver.Handler
 }
-
-const REDIS_URL_VARIABLE = "REDIS_URL"
 
 func redisURL() string {
 	url, ok := os.LookupEnv(REDIS_URL_VARIABLE)
@@ -36,6 +42,17 @@ func newRedisClient(h StatsHandler) *redis.Client {
 	return redis.NewClient(options)
 }
 
+func addToLog(c *redis.Client, l log_json.LogJSON, logMaxSize int) {
+	zadd := c.ZAdd(LOG_SET, redis.Z{float64(l.Time.Unix()), l.String()})
+	if zadd.Err() != nil {
+		log.Fatal(zadd.Err())
+	}
+	zrr := c.ZRemRangeByRank(LOG_SET, 0, int64(-logMaxSize-1))
+	if zrr.Err() != nil {
+		log.Fatal(zrr.Err())
+	}
+}
+
 func (h StatsHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) (int, error) {
 	c := newRedisClient(h)
 	l := log_json.NewLogJSON(r)
@@ -43,9 +60,6 @@ func (h StatsHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) (int, er
 	if incr.Err() != nil {
 		log.Fatal(incr.Err())
 	}
-	zadd := c.ZAdd("LOG", redis.Z{float64(l.Time.Unix()), l.String()})
-	if zadd.Err() != nil {
-		log.Fatal(zadd.Err())
-	}
+	addToLog(c, l, h.LogMaxSize)
 	return h.Next.ServeHTTP(w, r)
 }
