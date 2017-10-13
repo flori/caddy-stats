e50c091252a720dbb9667191c55eb4d813fdf733
Florian Frank <flori@ping.de>
2017-10-13 11:21:24 +0200

    Add more documentation

--- log_json/log_json.go
+++ log_json/log_json.go
@@ -10,14 +10,18 @@ type LogJSON struct {
 	Request *http.Request
 }
 
+// NewLogJSON creates a LogJSON object from the given HTTP request.
 func NewLogJSON(request *http.Request) LogJSON {
 	return LogJSON{Time: time.Now(), Request: request}
 }
 
+// Target returns the target of an request in the form of hostname:portnumber
+// as a string.
 func (l LogJSON) Target() string {
 	return l.Request.Host + l.Request.URL.String()
 }
 
+// String returns the JSON representation of this LogJSON object as a string.
 func (l LogJSON) String() string {
 	return "{\"target\":\"" + l.Target() + "\",\"referer\":\"" +
 		l.Request.Referer() + "\",\"time\":\"" + l.Time.Format(time.RFC3339) +
